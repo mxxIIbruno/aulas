a68e629fa213013a91de52efeb9d056fd3786792
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-13 23:28:13 -0300

    Aula sobre modulo pillow

---
+++ Python - Modulos/aula43/main.py
@@ -0,0 +1,28 @@
+"""
+    Pillow: redimensionando imagens com Python
+    Essa biblioteca é o Photoshop do Python 😂
+"""
+from pathlib import Path
+from PIL import Image
+
+ROOT_FOLDER = Path(__file__).parent
+ORIGINAL = ROOT_FOLDER / 'img-helena.jpg'
+NEW_IMAGE = ROOT_FOLDER / 'new.jpg'
+
+pil_image = Image.open(ORIGINAL)
+width, height = pil_image.size
+# exif = pil_image.info['exif']
+
+# width     new_width
+# height    new_height
+
+new_width = 640
+new_height = round(height * new_width / width)
+
+new_image = pil_image.resize(size=(new_width, new_height))
+new_image.save(
+    NEW_IMAGE,
+    optimize=True,
+    quality=70,
+    # exif=exif
+)
