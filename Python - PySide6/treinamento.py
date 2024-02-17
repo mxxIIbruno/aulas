93456110b4672497b221b175ee83dab70a297412
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-16 23:27:28 -0300

    treinado busca de dados com dados pessoais com modulos

---
+++ Python - PySide6/treinamento.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import requests
+
+
+class MyClass:
+    def __init__(self, name, surname) -> None:
+        self.name = self.set_name(name)
+        self.surname = self.set_surname(surname)
+        self.age = None
+
+    def set_name(self, name: str):
+        return name.strip().title()
+
+    def set_surname(self, surname: str):
+        return surname.strip().title()
+
+    def set_age(self, day, month, year):
+        date = datetime.now()
+        birth = datetime(year, month, day)
+        self.age = relativedelta(date, birth).years
+
+    def consultar_cep(self, cep):
+        url = f'https://viacep.com.br/ws/{cep}/json'
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data_cep = response.json()
+            return data_cep
+        except requests.exceptions.HTTPError as erro_http:
+            print(f'Erro na requisição HTTP: {erro_http}')
+        except requests.exceptions.RequestException as erro_req:
+            print(f'Erro na requisição: {erro_req}')
+
+    def info(self, value):
+        data_cep = self.consultar_cep(value)
+
+        if data_cep:
+            print(f'Informações do CEP {value}:\n{data_cep}')
+        else:
+            print(f'Não foi possível obter informações para o CEP {value}')
+
+
+if __name__ == '__main__':
+    CEP = "01310-100"  # "00000-000"
+    my_object = MyClass(
+        "name", "surname"
+    )
+    my_object.set_age(  # Date of Birth
+        1,  # Day
+        1,  # Month
+        9999  # Year
+    )
+    my_object.consultar_cep(CEP)
+    my_object.info(CEP)
