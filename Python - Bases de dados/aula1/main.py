dc9a88d9889486ece9d66ba3bc7d5df89a2394d2
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-20 23:56:14 -0300

    Criando minha primeira tabela no SQLite (DBeaver opcional)

--- Python - Bases de dados/aula1/main.py
+++ Python - Bases de dados/aula1/main.py
@@ -1,15 +1,27 @@
 import sqlite3
 from pathlib import Path
 
+# Configurando o carminho do arquivo
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+
+# Configurando nome da tabela
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
 # SQL
-
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
 
 cursor.close()
 connection.close()
