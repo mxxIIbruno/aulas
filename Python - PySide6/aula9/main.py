e2be9ab26495ba29a71c0358b8bce38cc26e0bf7
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-20 22:31:16 -0300

    QObject e QThread: o problema!

--- Python - PySide6/aula9/main.py
+++ Python - PySide6/aula9/main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from PySide6.QtWidgets import QApplication, QWidget
 from workerui import Ui_myWidget
@@ -8,6 +9,19 @@ class MyWidget(QWidget, Ui_myWidget):
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.setupUi(self)
+
+        self.button1.clicked.connect(self.hardWork1)
+        self.button2.clicked.connect(self.hardWork2)
+
+    def hardWork1(self):
+        for i in range(5):
+            time.sleep(1)
+        self.label1.setText('1 terminado')
+
+    def hardWork2(self):
+        for i in range(5):
+            time.sleep(5)
+        self.label_2.setText('2 terminado')
 
 
 if __name__ == '__main__':
