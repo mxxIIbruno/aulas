db490c50f4eb6cee511eaae452c5ee0751d70000
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-03-14 14:05:49 -0300

    DELETE no SQLite do Python

--- Python - Bases de dados/aula1/main.py
+++ Python - Bases de dados/aula1/main.py
@@ -64,8 +64,26 @@ cursor.executemany(
 )
 connection.commit()
 
-cursor.close()
-connection.close()
-
 if __name__ == '__main__':
     print(sql)
+
+    cursor.execute(
+        f'DELETE FROM {TABLE_NAME} '
+        'WHERE id = "3"'
+    )
+    connection.commit()
+
+    cursor.execute(
+        f'DELETE FROM {TABLE_NAME} '
+        'WHERE id = "4"'
+    )
+    connection.commit()
+
+    cursor.execute(f'SELECT * FROM {TABLE_NAME}')
+
+    for row in cursor.fetchall():
+        _id, name, weight = row
+        print(_id, name, weight)
+
+    cursor.close()
+    connection.close()
