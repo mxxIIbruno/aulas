3a1cffebc1a45611fddaf5c9f70c93979d380beb
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-03-15 15:32:41 -0300

    PyMySQL - um cliente MySQL feito em Python Puro

---
+++ Python - Bases de dados/aula3/main.py
@@ -0,0 +1,6 @@
+"""
+    PyMySQL - um cliente MySQL feito em Python Puro
+    Doc: https://pymysql.readthedocs.io/en/latest/
+    Pypy: https://pypi.org/project/pymysql/
+    GitHub: https://github.com/PyMySQL/PyMySQL
+"""
