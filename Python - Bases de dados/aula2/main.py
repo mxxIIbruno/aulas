8c707c07afa83241909c10892abaf4315db3de67
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-03-15 16:07:54 -0300

    CREATE TABLE para criar tabela com PRIMARY KEY no PyMySQL

--- Python - Bases de dados/aula2/main.py
+++ Python - Bases de dados/aula2/main.py
@@ -4,20 +4,28 @@
     Pypy: https://pypi.org/project/pymysql/
     GitHub: https://github.com/PyMySQL/PyMySQL
 """
+
 import os
 
-import pymysql
+import pymysql  # type: ignore
 import dotenv
 
 dotenv.load_dotenv()
 
-connection = pymysql.connect(host=os.environ['MYSQL_HOST'],
-                             user=os.environ['MYSQL_USER'],
-                             passwd=os.environ['MYSQL_PASSWORD'],
-                             database=os.environ['MYSQL_DATABASE'])
+connection = pymysql.connect(
+    host=os.environ["MYSQL_HOST"],
+    user=os.environ["MYSQL_USER"],
+    passwd=os.environ["MYSQL_PASSWORD"],
+    database=os.environ["MYSQL_DATABASE"],
+)
 with connection:
-    cursor = connection.cursor()
+    with connection.cursor() as cursor:
+        cursor.execute("CREATE TABLE IF NOT EXISTS customers ("
+                       "id INT NOT NULL AUTO_INCREMENT, "
+                       "nome VARCHAR(50) NOT NULL, "
+                       "idade INT NOT NULL, "
+                       "PRIMARYKEY (id)"
+                       ")")
 
-    # SQL
-    print(cursor)
-    ...
+        print(cursor)
+        ...
